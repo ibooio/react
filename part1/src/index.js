e9796146f4dde54eac3ba9f09087815adc808bf9
ibooio <iboosocial@gmail.com>
2021-06-21 16:08:49 -0300

    Update index.js

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+
+const Hello =  ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const Header = (props) => {
   console.log(props)
@@ -17,39 +38,52 @@ const Total = (props) => {
 }
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
 
-const course = {
-  name: 'Half Stack application development',
-  parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  const name="Pedro";
+  const age = 35;
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
+      <Display counter={counter}/>
+      <Button handleClick={increaseByOne} text="plus"/>
+      <Button handleClick={decreaseByOne} text="minus"/>
+      <Button handleClick={setToZero} text="zero"/>
+      <Hello name={name} age={age}/>
       <Header course={course}/>
       <Content parts={course.parts}/>
       <Total parts={course.parts}/>
     </div>
   )
 }
-ReactDOM.render(<App />, document.getElementById('root'))
 
-
-
-
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
 
 /*
 const App = () => {
